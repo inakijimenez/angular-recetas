c23f9278ca182c7af24b91448d5cc8b3b198dcbe
Curso <Curso@IPARTEKAULA.COM>
2018-03-13 09:02:57 +0100

    concesionario rest

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import {HttpClientModule} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 //Modulo para routing
 import { AppRouter } from './app.routes';
